8d6da8b59172e266a7e7ff2984ed26f14f49e8ac
kimihiro-abe <kimihiro.abe@gmail.com>
2021-07-24 23:19:46 +0900

    Translate.javaの更新

--- boushuu_dialect_translation/Translate.java
+++ boushuu_dialect_translation/Translate.java
@@ -5,12 +5,12 @@ public class Translate { //翻訳機能のみを収録したクラス
 	public static String trans_txt; //翻訳語テキストを格納する変数
 
 	public static void Translate_main() { //辞書機能と翻訳機能を分割した。
-		String src =InputIF.src; //InputOutクラスのクラス変数srcを、このクラスで使用する変数に代入。
+		String src =InputIF.src; //InputIFクラスのクラス変数srcを、このクラスで使用する変数に代入。
 
 		//翻訳セクション
 		for (String word : Dictionary.dic_keys) { //キーを１つ取り出してwordに代入。
 			src = src.replace(word, Dictionary.dic.get(word));	//置換処理部。
-									//↑辞書のkeyに対応する語をvalue
+									//↑key(標準語)に対応する語をvalue(房州弁)で置換する
 		}
 		trans_txt = src; //翻訳結果を、このクラスのクラス変数trans_txtに代入。
 	}
